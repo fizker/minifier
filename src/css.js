96c9a67a043b5257c9067137150b48cc513dd1d1
Benjamin Horsleben <benjamin@fizkerinc.dk>
2012-08-28 22:27:13 +0200

    Updated output to not have comments - They will be stripped before writing to file anyway.
    Added basic support for handling urls in paths.

--- src/css.js
+++ src/css.js
@@ -7,13 +7,18 @@ var fs = require('fs')
   , format = require('util').format
   , utils = require('./utils')
   , importMatcher = /@import (url\()?["']?([^"'()]+)["']?\)?;/g
+  , urlMatcher = /url\((.*)\)/g
 
 function parse(file) {
 	var root = path.dirname(file)
 	  , content = utils.stripUTF8ByteOrder(fs.readFileSync(file, 'utf8'))
 
-	return content.replace(importMatcher, function(match, junk, file) {
-		var parsedFile = parse(path.join(root, file))
-		return format('/* %s */%s', match, parsedFile);
-	})
+	return content
+		.replace(importMatcher, function(match, junk, file) {
+			var parsedFile = parse(path.join(root, file))
+			return parsedFile
+		})
+		.replace(urlMatcher, function(match, url) {
+			return format('url(%s)', path.join(root, url))
+		})
 }
--- test/unit/css.parser.js
+++ test/unit/css.parser.js
@@ -18,48 +18,38 @@ describe('unit/css.parser.js', function() {
 			parser.parse('a/b')
 			expect(fs.readFileSync).to.have.been.calledWith('a/c')
 		})
+		it('should apply the path to any other urls as well', function() {
+			fs.readFileSync.withArgs('a/b').returns('a{background: url(c);}')
+			var result = parser.parse('a/b')
+			expect(result).to.equal('a{background: url(a/c);}')
+		})
 	})
 	describe('When parsing a file with different imports', function() {
 		var result
 		it('should work with strings with single quotes', function() {
 			fs.readFileSync.withArgs('a.css').returns("@import 'file';")
 			result = parser.parse('a.css')
-			expect(result).to.equal(
-				  "/* @import 'file'; */"
-				+ 'read file'
-			)
+			expect(result).to.equal('read file')
 		})
 		it('should work with strings with double quotes', function() {
 			fs.readFileSync.withArgs('a.css').returns('@import "file";')
 			result = parser.parse('a.css')
-			expect(result).to.equal(
-				  '/* @import "file"; */'
-				+ 'read file'
-			)
+			expect(result).to.equal('read file')
 		})
 		it('should work with urls without quotes', function() {
 			fs.readFileSync.withArgs('a.css').returns('@import url(file);')
 			result = parser.parse('a.css')
-			expect(result).to.equal(
-				  '/* @import url(file); */'
-				+ 'read file'
-			)
+			expect(result).to.equal('read file')
 		})
 		it('should work with urls with single quotes', function() {
 			fs.readFileSync.withArgs('a.css').returns("@import url('file');")
 			result = parser.parse('a.css')
-			expect(result).to.equal(
-				  "/* @import url('file'); */"
-				+ 'read file'
-			)
+			expect(result).to.equal('read file')
 		})
 		it('should work with urls with double quotes', function() {
 			fs.readFileSync.withArgs('a.css').returns('@import url("file");')
 			result = parser.parse('a.css')
-			expect(result).to.equal(
-				  '/* @import url("file"); */'
-				+ 'read file'
-			)
+			expect(result).to.equal('read file')
 		})
 	})
 	describe('When calling parse() with a css file with no imports', function() {
