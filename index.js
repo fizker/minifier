2972f25bc3b87a9a80fa92d60f5061f7bfa80499
Benjamin Horsleben <benjamin@fizkerinc.dk>
2012-08-31 20:05:42 +0200

    Added templating-options to index.js.

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ var program = require('commander')
   , sqwish = require('sqwish')
   , uglify = require('uglify-js')
   , stripUTF8ByteOrder = require('./src/utils').stripUTF8ByteOrder
+  , generateOutput = require('./src/utils').generateOutputName
 
   , glob = require('glob-whatev')
 
   , input
   , output
+  , template
 
 program
 	.version('0.2.0')
 	.option('-o, --output [file]', 'The output file')
+	.option('-t, --output-template [template]', 'A template for building the output file')
 	.usage('[--output file] path/to/input')
 	.parse(process.argv)
 
@@ -25,58 +28,64 @@ if(!input) {
 	process.exit()
 }
 
+output = program.output
+template = program.outputTemplate
+
+if(output && template) {
+	console.log(
+		  'It does not make sense to provide both --output and '
+		+ '--output-template options. Please choose one.'
+	)
+	process.exit(1)
+}
+
 if(fs.statSync(input).isDirectory()) {
-	glob.glob(path.join(input, '**/*.js')).forEach(function(filepath) {
-		handleInput(filepath, filepath.replace(/\.(css|js)$/, '.min.$1'))
-	})
-	glob.glob(path.join(input, '**/*.css')).forEach(function(filepath) {
-		handleInput(filepath, filepath.replace(/\.(css|js)$/, '.min.$1'))
-	})
+	glob.glob(path.join(input, '**/*.js')).forEach(handleInput)
+	glob.glob(path.join(input, '**/*.css')).forEach(handleInput)
 
 	console.log('done minifying all files in %s.', input)
 	process.exit()
 }
 
 
-output = program.output || input.replace(/\.(css|js)$/, '.min.$1')
-
-handleInput(input, output)
+handleInput(input)
 
 console.log('done minifying %s. The minified file can be found at %s.', input, output)
 
-
-function handleInput(input, output) {
+function handleInput(input) {
 	if(!/\.(js|css)$/.test(input)) {
 		console.log('Please reference a file with the extension .js or .css. You referenced <%s>', input)
 		process.exit(1)
 	}
 
 	if(/\.js$/.test(input)) {
-		js(input, output)
+		js(input)
 	} else {
-		css(input, output)
+		css(input)
 	}
 }
 
-function js(input, output) {
+function js(input) {
 	var max = fs.readFileSync(input, 'utf8')
 	  , max = stripUTF8ByteOrder(max)
 	  , ast = uglify.parser.parse(max)
 	  , ast = uglify.uglify.ast_mangle(ast)
 	  , ast = uglify.uglify.ast_squeeze(ast)
 	  , min = uglify.uglify.gen_code(ast, {})
+	  , renderedOutput = generateOutput(input, min, output || template)
 
-	fs.writeFileSync(output, min)
+	fs.writeFileSync(renderedOutput, min)
 }
 
-function css(input, output) {
+function css(input) {
 	var parser = require('./src/css')
 	  , inDir = path.dirname(input)
-	  , outDir = path.dirname(output)
+	  , outDir = path.dirname(output || input)
 	  , root = path.join(inDir, path.relative(inDir, outDir))
 	  , max = parser.parse(input, root)
 	  , max = stripUTF8ByteOrder(max)
 	  , min = sqwish.minify(max, false)
+	  , renderedOutput = generateOutput(input, min, output || template)
 
-	fs.writeFileSync(output, min)
+	fs.writeFileSync(renderedOutput, min)
 }
