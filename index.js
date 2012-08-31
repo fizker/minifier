4af038c49734836e31402455a2b06b4dac2501b7
Benjamin Horsleben <benjamin@fizkerinc.dk>
2012-08-31 18:04:35 +0200

    Added simple globbing for compressing all css/js files in a dir. - They are named for the default template atm.

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ var program = require('commander')
   , uglify = require('uglify-js')
   , stripUTF8ByteOrder = require('./src/utils').stripUTF8ByteOrder
 
+  , glob = require('glob-whatev')
+
   , input
   , output
-  , inDir
-  , outDir
 
 program
 	.version('0.2.0')
@@ -24,23 +24,39 @@ if(!input) {
 	program.parse(['bla', 'bla', '--help'])
 	process.exit()
 }
-inDir = path.dirname(input)
 
-if(!/\.(js|css)$/.test(input)) {
-	console.log('Please reference a file with the extension .js or .css. You referenced <%s>', input)
+if(fs.statSync(input).isDirectory()) {
+	glob.glob(path.join(input, '**/*.js')).forEach(function(filepath) {
+		handleInput(filepath, filepath.replace(/\.(css|js)$/, '.min.$1'))
+	})
+	glob.glob(path.join(input, '**/*.css')).forEach(function(filepath) {
+		handleInput(filepath, filepath.replace(/\.(css|js)$/, '.min.$1'))
+	})
+
+	console.log('done minifying all files in %s.', input)
 	process.exit()
 }
 
-output = program.output || input.replace(/\.(css|js)$/, '.min.$1')
-outDir = path.dirname(output)
 
-if(/\.js$/.test(input)) {
-	js(input, output)
-} else {
-	css(input, output)
-}
+output = program.output || input.replace(/\.(css|js)$/, '.min.$1')
+
+handleInput(input, output)
 
 console.log('done minifying %s. The minified file can be found at %s.', input, output)
+
+
+function handleInput(input, output) {
+	if(!/\.(js|css)$/.test(input)) {
+		console.log('Please reference a file with the extension .js or .css. You referenced <%s>', input)
+		process.exit(1)
+	}
+
+	if(/\.js$/.test(input)) {
+		js(input, output)
+	} else {
+		css(input, output)
+	}
+}
 
 function js(input, output) {
 	var max = fs.readFileSync(input, 'utf8')
@@ -55,6 +71,8 @@ function js(input, output) {
 
 function css(input, output) {
 	var parser = require('./src/css')
+	  , inDir = path.dirname(input)
+	  , outDir = path.dirname(output)
 	  , root = path.join(inDir, path.relative(inDir, outDir))
 	  , max = parser.parse(input, root)
 	  , max = stripUTF8ByteOrder(max)
