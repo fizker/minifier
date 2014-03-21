3182e2839f759334f8344651b1bd5ceb4bca7067
Benjamin Horsleben <benjamin@fizkerinc.dk>
2014-03-22 00:47:35 +0100

    Bumped dependencies.

--- test/common.js
+++ test/common.js
@@ -2,6 +2,6 @@ global.chai = require('chai');
 global.expect = chai.expect;
 
 global.fzkes = require('fzkes')
-chai.use(fzkes.chai)
+chai.use(fzkes)
 
 require('finc-chai-helpers').addMethods(chai);
